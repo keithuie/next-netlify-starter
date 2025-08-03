db2867c84aadd365309efa7fb76736ec21f322fc
keithuie <keith@uie.group>
2025-08-03 17:06:54 +0100

    Gen2 fonts _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,30 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* This links to Google Fonts to load the Manrope font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        
         {/* This script loads Tailwind CSS for the entire application */}
         <script src="https://cdn.tailwindcss.com"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              tailwind.config = {
+                theme: {
+                  extend: {
+                    fontFamily: {
+                      sans: ['Manrope', 'sans-serif'],
+                    },
+                  },
+                },
+              };
+            `,
+          }}
+        />
       </Head>
-      <body className="bg-gray-800">
+      <body className="bg-gray-900">
         <Main />
         <NextScript />
       </body>
